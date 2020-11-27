6039149b332d2290c6048116b02ba70c0e61f016
Roland Levy <rolandjlevy@gmail.com>
2020-11-27 22:31:57 +0000

    All quotes load at start

--- public/snow.js
+++ public/snow.js
@@ -8,40 +8,44 @@ let tooltipOn = false;
 $$('.snowflake').forEach(item => {
   item.addEventListener('mouseenter', (e) => {
     if (!tooltipOn) {
-      getQuote(e.target.id).then(result => {
-        $('.tooltip').textContent = result;
-        $('.tooltip').classList.add('active');
-      });
+      const pos = Number(e.target.id);
+      const { text, author } = quotes[pos];
+      const quote = `"${text}"` + (author ? ` ${author}` : '');
+      $('.tooltip').textContent = quote;
+      $('.tooltip').classList.add('active');
       tooltipOn = true;
+      e.currentTarget.style.animationPlayState = 'paused';
+      e.currentTarget.style.opacity = 1;
     }
-    e.currentTarget.style.animationPlayState = 'paused';
   });
   item.addEventListener('mouseleave', (e) => {
     tooltipOn = false;
     $('.tooltip').classList.remove('active');
     e.currentTarget.style.animationPlayState = 'running';
+    e.currentTarget.style.opacity = getVar(e.currentTarget, '--opacity');
   });
 });
 
-const getQuote = (id) => {
+const getVar = (elem, varName) => {
+  return getComputedStyle(elem).getPropertyValue(varName).trim();
+}
+
+const setVar = (elem, varName, value) => {
+  elem.style.setProperty(varName, value);
+}
+
+const fetchQuotes = () => {
   return new Promise((resolve, reject) => {
     return fetch(quotesApiUrl)
-      .then(res => res.json())
-      .then(data => {
-        const pos = Number(id);
-        const { text, author } = filteredData(data)[pos];
-        const quote = `"${text}"` + (author ? ` ${author}` : '');
-        resolve(quote);
-      });
+    .then(res => res.json())
+    .then(data => {
+      const quotes = filteredQuotes(data);
+      resolve(quotes);
+    });
   });
 }
 
-// getQuote(e.target.id).then(result => {
-//   $('.tooltip').textContent = result;
-//   $('.tooltip').classList.add('active');
-// });
-
-const filteredData = (arr) => {
+const filteredQuotes = (arr) => {
   return arr.reduce((acc, item) => {
     if (item.author && item.author.toLowerCase().includes('dalai') == false) {
       acc.push(item);
@@ -49,3 +53,8 @@ const filteredData = (arr) => {
     return acc;
   }, []);
 }
+
+let quotes;
+fetchQuotes().then(result => {
+  quotes = result;
+});
