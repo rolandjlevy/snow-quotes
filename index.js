c53623d646d770c6a6107b3e5cc8e7ace8f1b8fc
Roland Levy <rolandjlevy@gmail.com>
2020-11-28 10:39:50 +0000

    Created get route and middleware for req.body

--- index.js
+++ index.js
@@ -3,14 +3,31 @@ const app = express();
 const path = require('path');
 const pug = require('pug');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const port = process.env.PORT;
+const baseUrl = process.env.API_URL;
 
 const Colours = require('./Colours');
 const colours = new Colours();
 
-const port = process.env.PORT;
-const baseUrl = process.env.API_URL;
+function rawBodyMiddleware(req, res, next) {
+  req.rawBody = '';
+  req.on('data', (chunk) => {
+    req.rawBody += chunk;
+  });
+  next();  
+}
 
-app.use(express.urlencoded({ extended: true }));
+app.use(rawBodyMiddleware);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function afterMiddleware(req, res, next) {
+  res.locals.data = req.rawBody ? req.body : req.query;
+  next();
+}
+app.use(afterMiddleware);
+
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -19,8 +36,16 @@ app.get('/', (req, res) => {
   res.render('index.pug', {});
 });
 
+app.get('/snow', (req, res) => {
+  renderSnow(req, res);
+});
+
 app.post('/snow', (req, res) => {
-  const { letters, quantity, colour } = req.body;
+  renderSnow(req, res);
+});
+
+const renderSnow = ({req, res}) => {
+  const { letters, quantity, colour } = res.locals.data;
   res.render('snow.pug', {
     letters,
     quantity,
@@ -28,7 +53,7 @@ app.post('/snow', (req, res) => {
     colours,
     baseUrl
   });
-});
+}
 
 app.listen(() => {
   console.log('Listening on port', port || 3000);
