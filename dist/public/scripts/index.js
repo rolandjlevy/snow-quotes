eba1a8bb74e961d81316d34c04be37a9fa9ed207
Roland Levy <rolandjlevy@gmail.com>
2021-01-01 19:26:57 +0000

    Updated year in footer

--- dist/public/scripts/index.js
+++ dist/public/scripts/index.js
@@ -21,8 +21,7 @@ var withinFontFaceSet = function withinFontFaceSet(str) {
 
 var sharing = new _Sharing2.default();
 var testUrl = 'https://snow-quotes.rolandjlevy.repl.co/snow?letters=HelloJhey&quantity=100&colour=%23e1e1ee&multicolour=0';
-// console.log('testUrl: ', testUrl);
-sharing.test(testUrl);
+// sharing.test(testUrl);
 
 var toast = new _Toast2.default();
 toast.init();
